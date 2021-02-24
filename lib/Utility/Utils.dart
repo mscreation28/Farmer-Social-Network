b1abb037f40f3be1a53db090abcdaf3ab532504e
Nitant Joshi <nitantjoshi20@gmail.com>
2021-02-24 18:02:00 +0530

    Messages modified

--- lib/Utility/Utils.dart
+++ lib/Utility/Utils.dart
@@ -1,5 +1,5 @@
 class Utils{
-    static const String BASE_URL = "http://192.168.2.100:3000/";
+    static const String BASE_URL = "http://192.168.43.99:3000/";
     static const String USER_ID = "userId";
     static const String TOKEN = "token";
     static const String USER = "user";
